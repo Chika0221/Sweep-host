486c49d0615274244f8d9d6f6a05f97687e5b091
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-05-29 15:59:11 +0900

    main関数を非同期に変更し、Firebaseの初期化を追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
+import 'package:sweep_host/firebase_options.dart';
 import 'package:sweep_host/pages/main_page.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final app = MyApp();
   final providerScope = ProviderScope(child: app);
   runApp(providerScope);
