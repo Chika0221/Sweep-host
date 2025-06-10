1a0c0b730648ff719d4ea35f47f620409261222c
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-06-11 01:07:38 +0900

    ログインページに新しいアイコンを追加し、pubspec.yamlでアイコンのパスを更新

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LoginPage extends HookConsumerWidget {
@@ -13,15 +14,40 @@ class LoginPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       children: [
-        Expanded(child: Container(color: Colors.red)),
-        Container(
-          transform: Matrix4.rotationZ(90 * pi / 180),
-          transformAlignment: Alignment.center,
-          width: 50,
-          height: double.infinity,
-          child: Icon(Icons.arrow_upward),
+        Expanded(
+          child: Center(
+            child: Image.asset(
+              "assets/icons/sweep-hostIcon.png",
+              width: MediaQuery.of(context).size.width * 0.3,
+              height: MediaQuery.of(context).size.width * 0.3,
+            ),
+          ),
         ),
-        Expanded(child: Container(color: Colors.blue)),
+        SizedBox(
+          width: 500,
+          child: Container(
+            margin: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primaryContainer,
+                  Theme.of(context).colorScheme.primary,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                /*ログインフォーム*/
+                Text("ログイン",style: Theme.of(context).textTheme.),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
