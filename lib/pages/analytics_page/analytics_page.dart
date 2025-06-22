ad40c58b58c4b2ba1f89989ee7ff9ad1e7b39b9f
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-06-23 00:32:25 +0900

    AnalyticsPageにユーザーランキングチャートを追加し、ユーザー数データを視覚化する機能を実装

--- lib/pages/analytics_page/analytics_page.dart
+++ lib/pages/analytics_page/analytics_page.dart
@@ -9,6 +9,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sweep_host/pages/analytics_page/analytics_info_card.dart';
 import 'package:sweep_host/pages/analytics_page/bento_flame.dart';
 import 'package:sweep_host/pages/analytics_page/post_peek_chart.dart';
+import 'package:sweep_host/pages/analytics_page/user_ranking_chart.dart';
 import 'package:sweep_host/pages/dashboard_page/post_data_chart.dart';
 import 'package:sweep_host/states/user_stream_provider.dart';
 
@@ -32,9 +33,9 @@ class AnalyticsPage extends HookConsumerWidget {
         columnGap: 12,
         rowGap: 12,
         areas: '''
-          userCount j k k l
+          userCount i i i i 
           post post post peek peek
-          f f f f f 
+          user user user user user 
         ''',
 
         columnSizes: [1.0.fr, 1.0.fr, 0.6.fr, 0.4.fr, 1.0.fr],
@@ -53,9 +54,7 @@ class AnalyticsPage extends HookConsumerWidget {
           // gridArea("l").containing(BentoFlame(child: AnalyticsInfoCard())),
           gridArea("post").containing(BentoFlame(child: PostDataChart())),
           gridArea("peek").containing(BentoFlame(child: PostPeekChart())),
-          gridArea(
-            "f",
-          ).containing(BentoFlame(child: Container(color: Colors.orange))),
+          gridArea("user").containing(BentoFlame(child: UserRankingChart())),
         ],
       ),
     );
---
+++ lib/pages/analytics_page/user_ranking_chart.dart
@@ -0,0 +1,94 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:fl_chart/fl_chart.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// Project imports:
+import 'package:sweep_host/classes/profile.dart';
+import 'package:sweep_host/states/user_stream_provider.dart';
+
+class UserRankingChart extends HookConsumerWidget {
+  const UserRankingChart({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userStream = ref.watch(userStreamProvider);
+
+    return userStream.when(
+      data: (data) {
+        List<BarChartGroupData> barData = [];
+
+        List<Profile> sortedData = List.from(data)
+          ..sort((a, b) => b.cumulativePoint.compareTo(a.cumulativePoint));
+
+        for (var i = 1; i <= 20; i++) {
+          double value = 0;
+          barData.add(
+            BarChartGroupData(
+              x: i,
+              barRods: [
+                BarChartRodData(
+                  toY: sortedData[i - 1].cumulativePoint.toDouble(),
+                  width: 30,
+                  color: Colors.lightGreen,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ],
+              groupVertically: true,
+            ),
+          );
+        }
+
+        return BarChart(
+          BarChartData(
+            barGroups: barData,
+            barTouchData: BarTouchData(
+              enabled: true,
+              touchTooltipData: BarTouchTooltipData(
+                getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                  return BarTooltipItem(
+                    "${rod.toY}",
+                    Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      color: Colors.lightGreen,
+                    ),
+                  );
+                },
+                getTooltipColor: (group) {
+                  return Colors.transparent;
+                },
+              ),
+            ),
+            gridData: FlGridData(show: false),
+            titlesData: FlTitlesData(
+              rightTitles: AxisTitles(
+                sideTitles: SideTitles(showTitles: false),
+              ),
+              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              bottomTitles: AxisTitles(
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  getTitlesWidget: (value, meta) {
+                    return Text("$value");
+                  },
+                ),
+              ),
+            ),
+            borderData: FlBorderData(show: false),
+          ),
+        );
+      },
+      loading: () => Center(child: CircularProgressIndicator()),
+      error:
+          (error, stackTrace) => Center(
+            child: Text(
+              'Error loading data: $error',
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                color: Theme.of(context).colorScheme.error,
+              ),
+            ),
+          ),
+    );
+  }
+}
